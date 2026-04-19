29591f11243394460a35478dff5ec906432f6029
gautamrudakiyatech <rudakiyagautamj@gmail.com>
2026-04-19 21:29:16 +0530

    Make total new UI with admin login 18-19/04/26 new updation

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,10 +1,10 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  const projectId = process.env.FIREBASE_ADMIN_PROJECT_ID || process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+  const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
+  const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY
+    ? process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n')
     : undefined;
 
   if (projectId && clientEmail && privateKey) {
